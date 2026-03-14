ddcbd40905a4837ffc73cabf9363af11cc4fe387
MarkusUA11 <markre1610@gmail.com>
2026-03-14 13:27:19 +0200

    Add files via upload

--- mainwindow.py
+++ mainwindow.py
@@ -1,9 +1,19 @@
 from customtkinter import *
-
+import threading
+import base64
+import io
+import os
+from PIL import Image
+from socket import socket, AF_INET, SOCK_STREAM
 
 class MainWindow(CTk):
-    def __init__(self):
+    def __init__(self,sock, username):
         super().__init__()
+
+        self.sock = sock
+        self.username = username
+        self.geometry('400x600')
+        self.title("Chat Cilent")
 
 
         self.geometry('400x600')
@@ -17,7 +27,7 @@ class MainWindow(CTk):
         self.menu_frame.place(x=0, y=0)
         self.is_show_menu = False
         self.speed_animate_menu = -20
-        self.btn = CTkButton(self, text='▶️', width=30)
+        self.btn = CTkButton(self, text='▶️', width=30,command =  self.toggle_menu)
         self.btn.place(x=0, y=0)
 
 
@@ -108,7 +118,75 @@ class MainWindow(CTk):
         else:
             CTkLabel(message_frame, text=message, wraplength=wrapleng_size,text_color='white', image=img, compound='top',justify='left').pack(padx=10,pady=5)
 
+    def send_message(self):
+        message = self.message_entry.get()
+        if message:
+            self.add_message(f"(self.username): {message}")
+            data = f"TEXT@{self.username}@{message}\n"
+            try:
+                self.sock.sendall(data.encode())
+            except:
+                pass
+            self.message_entry.delete(0, END)
+    
+    def recv_message(self):
+        buffer = ""
+        while True:
+            try:
+                chunk = self.sock.recv(4096)
+                if not chunk:
+                    break
+                buffer += chunk.decode(errors='ignore')
+
+                while "\n" in buffer:
+                    line, buffer = buffer.split('\n',1)
+                    self.handle_line(line.strip())
+            except:
+                break
+            self.sock.close()
+
+    def handle_line(self,line):
+        if not line:
+            return
+        parts = line.split("@", 3)
+        msg_type = parts[0]
+
+        if msg_type == "TEXT":
+            if len(parts) == 3:
+               author = parts[1]
+               message = parts[2]
+               self.add_message(f"{author}:{message}")
+            elif msg_type == "IMAGE":
+                if len(parts) >= 4:
+                    author = parts[1]
+                    filename = parts[2]
+                    b64_img = parts[3]
+                    try:
+                        img_data = base64.b64decode(b64_img)
+                        pil_img = Image.open(io).BytesIO((img_data))
+                        ctk_img  = CTkImage(pil_img, size=(300,300))
+                        self.add_message(f"{author} надіслав(ла) зображення: {filename}",img=ctk_img)
+                    except Exception as e:
+                        self.add_message(f"Помилка відображення зображення: {e}")
+            else:
+                 self.add_message(line)
+
+        def open_image(self):
+            file_name = filedialog.askopenfilename()
+            if not file_name:
+                return
+            try:
+                with open(file_name, "rb") as f:
+                    raw = f.read
+                b64_data = base64.b64encode(raw).decode()
+                short_name = os.path.basename(file_name)
+                data = f"IMAGE@{self.username}@{short_name}@{b64_data}\n"
+                self.sock.sendall(data.encode())
+                self.add_message("", CTkImage(light_image=Image.open(file_name),size=(300,300)))
+            except Exception as e:
+                self.add_message(f"Не вдалось надіслати зображення: {e}") 
+
+
+
 main = MainWindow()
 main.mainloop()
-
-    
